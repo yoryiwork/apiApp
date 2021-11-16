fa915d7b547669b3d19ba1fb0a67d80b553d74d0
yoryiwork <87830166+yoryiwork@users.noreply.github.com>
2021-11-16 14:09:59 -0600

    Modulo de rutas de clavesSat

--- src/app/claves-sat/claves-sat-routing.module.ts
+++ src/app/claves-sat/claves-sat-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ListadoComponent } from './pages/listado/listado.component';
 
-
+const rutas: Routes = [
+  {
+    path: '',
+    component: ListadoComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild( rutas )
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class ClavesSatRoutingModule { }
